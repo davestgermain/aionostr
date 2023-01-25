d05d520e1d474cbdad1ad8297d2bb0037a762b2a
Dave St.Germain <dcs@adullmoment.com>
2023-01-25 15:44:56 -0500

    implement reconnect

--- aionostr/relay.py
+++ aionostr/relay.py
@@ -2,13 +2,27 @@ import asyncio
 import secrets
 import time
 import sys
-from collections import defaultdict
-from websockets import connect
+from contextlib import asynccontextmanager
+from collections import defaultdict, namedtuple
+from websockets import connect, exceptions
 from .event import Event
+
 try:
     from rapidjson import dumps, loads
 except ImportError:
     from json import dumps, loads
+
+if hasattr(asyncio, 'timeout'):
+    timeout = asyncio.timeout
+else:
+    # python < 3.11 does not have asyncio.timeout
+    # rather than re-implement it, we'll just do nothing
+    @asynccontextmanager
+    async def timeout(duration):
+        yield
+
+
+Subscription = namedtuple('Subscription', ['filters','queue'])
 
 
 class Relay:
@@ -19,17 +33,33 @@ class Relay:
         self.url = url
         self.ws = None
         self.receive_task = None
-        self.subscriptions = defaultdict(asyncio.Queue)
+        self.subscriptions = defaultdict(lambda: Subscription(filters=[], queue=asyncio.Queue()))
         self.event_adds = asyncio.Queue()
         self.notices = asyncio.Queue()
         self.challenge = None
         self.verbose = verbose
         self.origin = origin or url
 
-    async def connect(self):
-        self.ws = await connect(self.url, origin=self.origin)
-        self.receive_task = asyncio.create_task(self._receive_messages())
+    async def connect(self, retries=5):
+        for i in range(retries):
+            try:
+                self.ws = await connect(self.url, origin=self.origin)
+            except:
+                await asyncio.sleep(0.2 * i)
+            else:
+                break
+        else:
+            raise Exception(f"Cannot connect to {self.url}")
+        if self.receive_task is None:
+            self.receive_task = asyncio.create_task(self._receive_messages())
         await asyncio.sleep(0.01)
+
+    async def reconnect(self):
+        await self.connect(20)
+        for sub_id, sub in self.subscriptions.items():
+            if self.verbose:
+                sys.stderr.write(f'resubscribing to {sub.filters}')
+            await self.send(["REQ", sub_id, *sub.filters])
 
     async def close(self):
         self.receive_task.cancel()
@@ -38,14 +68,16 @@ class Relay:
     async def _receive_messages(self):
         while True:
             try:
-                message = await self.ws.recv()
+                async with timeout(30.0):
+                    message = await self.ws.recv()
+
                 if self.verbose:
                     sys.stderr.write(message + '\n')
                 message = loads(message)
                 if message[0] == 'EVENT':
-                    await self.subscriptions[message[1]].put(Event(**message[2]))
+                    await self.subscriptions[message[1]].queue.put(Event(**message[2]))
                 elif message[0] == 'EOSE':
-                    await self.subscriptions[message[1]].put(None)
+                    await self.subscriptions[message[1]].queue.put(None)
                 elif message[0] == 'OK':
                     await self.event_adds.put(message)
                 elif message[0] == 'NOTICE':
@@ -56,9 +88,19 @@ class Relay:
                     sys.stderr.write(message)
             except asyncio.CancelledError:
                 return
+            except exceptions.ConnectionClosedError:
+                await self.reconnect()
+            except asyncio.TimeoutError:
+                continue
+            except:
+                import traceback; traceback.print_exc()
 
     async def send(self, message):
-        await self.ws.send(dumps(message))
+        try:
+            await self.ws.send(dumps(message))
+        except exceptions.ConnectionClosedError:
+            await self.reconnect()
+            await self.ws.send(dumps(message))
 
     async def add_event(self, event, check_response=False):
         if isinstance(event, Event):
@@ -69,10 +111,9 @@ class Relay:
             return response[1]
 
     async def subscribe(self, sub_id: str, *filters, queue=None):
-        if queue is not None:
-            self.subscriptions[sub_id] = queue
+        self.subscriptions[sub_id] = Subscription(filters=filters, queue=queue or asyncio.Queue())
         await self.send(["REQ", sub_id, *filters])
-        return self.subscriptions[sub_id]
+        return self.subscriptions[sub_id].queue
 
     async def unsubscribe(self, sub_id):
         await self.send(["CLOSE", sub_id])
