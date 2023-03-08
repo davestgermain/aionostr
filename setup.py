8ee0c3f1eaee1365eea1df614961bb95b5da0d2d
Dave St.Germain <dcs@adullmoment.com>
2023-03-07 20:27:26 -0500

    Bump version: 0.18.0 → 0.19.0

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/davestgermain/aionostr',
-    version='0.18.0',
+    version='0.19.0',
     zip_safe=False,
 )
